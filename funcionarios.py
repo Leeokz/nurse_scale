dd417a9e4704d25186713ff9ade7013dd412f566
Leandro Jesus <leandrosants159@gmail.com>
2026-05-26 00:02:54 -0300

    v1
    Lista com 16 funcionários.
    Função para adicionar mais um funcionário na lista.
    Exibiu a lista atualizada ID: 17 e cargo correto

---
+++ funcionarios.py
@@ -0,0 +1,101 @@
+funcionarios = [
+    {
+        "id": 1,
+        "nome": "João",
+        "cargo": "Enfermeiro"
+    },
+    {
+        "id": 2,
+        "nome": "Maria",
+        "cargo": "Enfermeira"
+    },
+    {
+        "id": 3,
+        "nome": "Carlos",
+        "cargo": "Enfermeiro"
+    },
+    {
+        "id": 4,
+        "nome": "Ana",
+        "cargo": "Enfermeira"
+    },
+    {
+        "id": 5,
+        "nome": "Leonardo",
+        "cargo": "Enfermeiro"
+    },
+    {
+        "id": 6,
+        "nome": "Julia",
+        "cargo": "Enfermeira"
+    },
+    {
+        "id": 7,
+        "nome": "Bruno",
+        "cargo": "Enfermeiro"
+    },
+    {
+        "id": 8,
+        "nome": "Carla",
+        "cargo": "Enfermeira"
+    },
+    {
+        "id": 9,
+        "nome": "Enzo",
+        "cargo": "Enfermeiro"
+    },
+    {
+        "id": 10,
+        "nome": "Jessica",
+        "cargo": "Enfermeira"
+    },
+    {
+        "id": 11,
+        "nome": "Gustavo",
+        "cargo": "Enfermeiro"
+    },
+    {
+        "id": 12,
+        "nome": "Fernanda",
+        "cargo": "Enfermeira"
+    },
+    {
+        "id": 13,
+        "nome": "Lucas",
+        "cargo": "Enfermeiro"
+    },
+    {
+        "id": 14,
+        "nome": "Mariana",
+        "cargo": "Enfermeira"
+    },
+    {
+        "id": 15,
+        "nome": "Ricardo",
+        "cargo": "Enfermeiro"
+    },
+    {
+        "id": 16,
+        "nome": "Sofia",
+        "cargo": "Enfermeira"
+    },]
+
+def func_list():
+    print("Lista de Funcionários:")
+    for funcionario in funcionarios:
+        print(f"ID: {funcionario['id']}, Nome: {funcionario['nome']}, Cargo: {funcionario['cargo']}")
+
+func_list()
+
+def add_funcionario(nome, cargo):
+    new_id = len(funcionarios) + 1
+    new_funcionario = {
+        "id": new_id,
+        "nome": nome,
+        "cargo": cargo
+    }
+    funcionarios.append(new_funcionario)
+    print(f"Funcionário {nome} adicionado com sucesso!")
+
+add_funcionario("Pedro", "Técnico de Enfermagem")
+func_list()
